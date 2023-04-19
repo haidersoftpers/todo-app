b05062f7a2f8ff48280610067cca6b530b574cbb
Haider abbas <haider@team.softpers.com>
2023-04-19 12:45:20 +0500

    added navbar

--- src/app/layout/HeaderNavbar.jsx
+++ src/app/layout/HeaderNavbar.jsx
@@ -12,7 +12,7 @@ const HeaderNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="">Todo App</Navbar.Brand>
+        <Navbar.Brand >Todo App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
